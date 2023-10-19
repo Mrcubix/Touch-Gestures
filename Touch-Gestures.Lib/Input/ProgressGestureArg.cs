bc7fc25b2168c4e4675164be8b20baf8e774975e
Mrcubix <tintamare91@gmail.com>
2023-10-19 13:36:34 +0200

    Added ProgressGestureArgs for INodeBasedGesture

---
+++ Touch-Gestures.Lib/Input/ProgressGestureArg.cs
@@ -0,0 +1,28 @@
+namespace TouchGestures.Lib.Input
+{
+    public class ProgressGestureArg : GestureEventArgs
+    {
+        #region Constructors
+
+        public ProgressGestureArg()
+        {
+            LastCompletedNodeIndex = -1;
+        }
+
+        public ProgressGestureArg(bool hasStarted, bool hasEnded, bool hasCompleted, int lastCompletedNodeIndex) : base(hasStarted, hasEnded, hasCompleted)
+        {
+            LastCompletedNodeIndex = lastCompletedNodeIndex;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        ///   The index of the last completed node in the <see cref="INodeBasedGesture" />.
+        /// </summary>
+        public int LastCompletedNodeIndex { get; set; }
+
+        #endregion
+    }
+}
