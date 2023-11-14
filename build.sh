79398dfe91354c8a6640d68e9c69ccf42af96c7a
Mrcubix <tintamare91@gmail.com>
2023-11-14 04:06:27 +0100

    update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,63 @@
 #!/bin/sh
 
+echo ""
+echo "Building Touch-Gestures"
+echo ""
+
 # build the plugin
-dotnet publish Touch-Gestures -c Release -p:noWarn='"NETSDK1138;VSTHRD200"' -o build/plugin
+dotnet publish Touch-Gestures -c Release -p:noWarn='"NETSDK1138;VSTHRD200"' -o temp/
+
+echo ""
+echo "Moving necessary files to build/plugin"
+echo ""
+
+mkdir build
+
+# check if directory exists
+if [ ! -d "build" ];
+then
+    echo "Failed to create the 'build' directory"
+    exit 1
+fi
+
+cd build
+
+rm -rf build/*
+
+mkdir plugin
+
+# check if directory exists
+if [ ! -d "plugin" ];
+then
+    echo "Failed to create the 'build/plugin' directory"
+    exit 1
+fi
+
+cd ..
+
+mv temp/Touch-Gestures.dll build/plugin/Touch-Gestures.dll
+mv temp/Touch-Gestures.pdb build/plugin/Touch-Gestures.pdb
+mv temp/Touch-Gestures.Lib.dll build/plugin/Touch-Gestures.Lib.dll
+mv temp/Touch-Gestures.Lib.pdb build/plugin/Touch-Gestures.Lib.pdb
+mv temp/OpenTabletDriver.External.Common.dll build/plugin/OpenTabletDriver.External.Common.dll
+mv temp/OpenTabletDriver.External.Common.pdb build/plugin/OpenTabletDriver.External.Common.pdb
+
+if [ $? -ne 0 ];
+then
+    echo "Failed to move files"
+    cd ../../
+    exit 1
+fi
+
+cd ./build/plugin
+
+zip -r Touch-Gestures.zip *
+
+cd ../../
+
+echo ""
+echo "Building Touch-Gestures.UX.Desktop"
+echo ""
 
 # build the desktop on linux & windows
 dotnet publish Touch-Gestures.UX.Desktop -c Release -p:noWarn='"NETSDK1138;VSTHRD200"' -r linux-x64 -o build/ux/linux
