7d71654c9656367c547acc61150d4be05c8c7b83
Mrcubix <tintamare91@gmail.com>
2024-02-01 20:15:29 +0100

    Early work at new Node canvas

--- Touch-Gestures.UX/Controls/Containers/NodeCanvas.axaml.cs
+++ Touch-Gestures.UX/Controls/Containers/NodeCanvas.axaml.cs
@@ -10,9 +10,38 @@ public class NodeCanvas : TemplatedControl
     public static readonly StyledProperty<ObservableCollection<NodeViewModel>> NodesProperty =
         AvaloniaProperty.Register<NodeCanvas, ObservableCollection<NodeViewModel>>(nameof(Nodes));
 
+    public static readonly StyledProperty<Size> MaxBoundsProperty =
+        AvaloniaProperty.Register<NodeCanvas, Size>(nameof(MaxBounds), new Size(223.52, 139.7));
+
     public ObservableCollection<NodeViewModel> Nodes
     {
         get => GetValue(NodesProperty);
         set => SetValue(NodesProperty, value);
+    }
+
+    public Size MaxBounds
+    {
+        get => GetValue(MaxBoundsProperty);
+        set => SetValue(MaxBoundsProperty, value);
+    }
+
+    protected override Size ArrangeOverride(Size maxSize)
+    {
+        // Do scaling here
+        var scaledWidth = maxSize.Height / MaxBounds.Height * MaxBounds.Width;
+        var scaledHeight = maxSize.Width / MaxBounds.Width * MaxBounds.Height;
+
+        if (scaledWidth > maxSize.Width)
+        {
+            Width = maxSize.Width;
+            Height = scaledHeight;
+        }
+        else
+        {
+            Width = scaledWidth;
+            Height = maxSize.Height;
+        }
+
+        return base.ArrangeOverride(maxSize);
     }
 }
