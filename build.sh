83a12dd5c97b8e2677ee582453f5f05ff9d60bcb
Gess1t <tintamare91@gmail.com>
2024-03-25 23:02:20 +0100

    Codefactor Cleanup

--- build.sh
+++ build.sh
@@ -11,13 +11,14 @@ echo ""
 echo "Moving necessary files to build/plugin"
 echo ""
 
-mkdir build
-
 # check if directory exists
-if [ ! -d "build" ];
+if ! mkdir build;
 then
-    echo "Failed to create the 'build' directory"
-    exit 1
+    if [ ! -d "build" ];
+    then
+        echo "Failed to create the 'build' directory"
+        exit 1
+    fi
 fi
 
 (
@@ -25,30 +26,43 @@ fi
 
     rm -rf build/*
 
-    mkdir plugin
-
     # check if directory exists
-    if [ ! -d "plugin" ];
+    if ! mkdir plugin;
     then
-        echo "Failed to create the 'build/plugin' directory"
-        exit 1
+        if [ ! -d "plugin" ];
+        then
+            echo "Failed to create the 'build/plugin' directory"
+            exit 1
+        fi
     fi
 )
 
-mv temp/Touch-Gestures.dll build/plugin/Touch-Gestures.dll
-mv temp/Touch-Gestures.pdb build/plugin/Touch-Gestures.pdb
-mv temp/Touch-Gestures.Lib.dll build/plugin/Touch-Gestures.Lib.dll
-mv temp/Touch-Gestures.Lib.pdb build/plugin/Touch-Gestures.Lib.pdb
-mv temp/OpenTabletDriver.External.Common.dll build/plugin/OpenTabletDriver.External.Common.dll
-mv temp/OpenTabletDriver.External.Common.pdb build/plugin/OpenTabletDriver.External.Common.pdb
-mv temp/Newtonsoft.Json.dll build/plugin/Newtonsoft.Json.dll
+main=("Touch-Gestures.dll"
+      "Touch-Gestures.Lib.dll" 
+      "OpenTabletDriver.External.Common.dll" 
+      "Newtonsoft.Json.dll"
+      "Touch-Gestures.pdb" 
+      "Touch-Gestures.Lib.pdb"
+      "OpenTabletDriver.External.Common.pdb")
 
-if [ $? -ne 0 ];
-then
-    echo "Failed to move files"
-    cd ../../
-    exit 1
-fi
+for file in "${main[@]}"
+do
+    # If file exist, move it to the build/plugin directory
+    if [ -f "temp/$file" ];
+    then
+        mv temp/$file build/plugin/$file
+    else
+        # If the file extension is not .pdb, then exit
+        if [[ $file != *.pdb ]];
+        then
+            echo "Failed to find $file"
+            exit 1
+        fi
+    fi
+done
+
+# remove the temp directory
+rm -rf temp
 
 cd ./build/plugin
 
@@ -61,10 +75,10 @@ echo "Building Touch-Gestures.UX.Desktop"
 echo ""
 
 # build the desktop on linux & windows
-dotnet publish Touch-Gestures.UX.Desktop -c Release -p:noWarn='"NETSDK1138;VSTHRD200"' -r linux-x64 -o build/ux/linux
-dotnet publish Touch-Gestures.UX.Desktop -c Release -p:noWarn='"NETSDK1138;VSTHRD200"' -r linux-arm64 -o build/ux/linux-arm64
-dotnet publish Touch-Gestures.UX.Desktop -c Release -p:noWarn='"NETSDK1138;VSTHRD200"' -r linux-arm -o build/ux/linux-arm
+platforms=("linux-x64" "linux-arm64" "linux-arm" "win-x64" "win-x86" "win-arm64")
 
-dotnet publish Touch-Gestures.UX.Desktop -c Release -p:noWarn='"NETSDK1138;VSTHRD200"' -r win-x64 -o build/ux/win
-dotnet publish Touch-Gestures.UX.Desktop -c Release -p:noWarn='"NETSDK1138;VSTHRD200"' -r win-x86 -o build/ux/win-x86
-dotnet publish Touch-Gestures.UX.Desktop -c Release -p:noWarn='"NETSDK1138;VSTHRD200"' -r win-arm64 -o build/ux/win-arm64
+for platform in "${platforms[@]}"
+do
+    # build the desktop on linux & windows
+    dotnet publish Touch-Gestures.UX.Desktop -c Release -p:noWarn='"NETSDK1138;VSTHRD200"' -r $platform -o build/ux/$platform
+done
