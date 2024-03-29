ee46dc2f13c64c53edc027fd2acb72dfcd010e8e
Gess1t <tintamare91@gmail.com>
2024-03-29 02:49:29 +0100

    Add a newline to space methods in Vector2Extension

--- Touch-Gestures.Lib/Extensions/Vector2Extensions.cs
+++ Touch-Gestures.Lib/Extensions/Vector2Extensions.cs
@@ -6,6 +6,7 @@ namespace TouchGestures.Lib.Extensions
     public static class Vector2Extensions
     {
         public static float Dot(this Vector2 vector, Vector2 other) => Vector2.Dot(vector, other);
+        
         public static Vector2 Round(this Vector2 vector, int decimals) 
         {
             decimal roundedX = Math.Round((decimal)vector.X, decimals);
