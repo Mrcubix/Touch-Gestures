691debaea1acbdc93dcc832678773ab39fe663ec
Gess1t <tintamare91@gmail.com>
2024-03-30 17:42:48 +0100

    Update build script to use more subsheels & also compute hash for the plugin & UXs

--- build.sh
+++ build.sh
@@ -64,21 +64,73 @@ done
 # remove the temp directory
 rm -rf temp
 
-cd ./build/plugin
+(
+    cd ./build/plugin
 
-zip -r Touch-Gestures.zip ./*
-
-cd ../../
+    zip -r Touch-Gestures.zip ./*
+)
 
 echo ""
 echo "Building Touch-Gestures.UX.Desktop"
 echo ""
 
 # build the desktop on linux & windows
-platforms=("linux-x64" "linux-arm64" "linux-arm" "win-x64" "win-x86" "win-arm64")
+platforms=("linux-x64" "linux-arm64" "linux-arm" "win-x64" "win-x86" "win-arm64" "osx-x64" "osx-arm64")
 
 for platform in "${platforms[@]}"
 do
     # build the desktop on linux & windows
     dotnet publish Touch-Gestures.UX.Desktop -c Release -p:noWarn='"NETSDK1138;VSTHRD200"' -r $platform -o build/ux/$platform
 done
+
+find ./build/ux -name "OpenTabletDriver*.pdb" -type f -delete
+
+# zip all the files
+(
+    cd ./build/ux
+
+    for f in *; do
+        if [ -d "$f" ]; then
+            echo "Zipping $f"
+            (
+                cd "$f"
+                zip -r "../Touch-Gestures.UX-$f.zip" *
+            )
+        fi
+    done
+)
+
+# Append all hashes to hashes.txt
+(
+    cd ./build
+
+    output="../hashes.txt"
+
+    (
+        cd ./plugin
+
+        # Compute Plugin Hash
+        sha256sum Touch-Gestures.zip > $output
+    )
+
+    echo "" >> hashes.txt
+
+    (
+        cd ./ux
+
+        # Compute all UX Hashes
+
+        for os in win linux osx; do
+            for arch in x64 x86 arm64; do
+
+                name="Touch-Gestures.UX-$os-$arch.zip"
+
+                echo "Computing $name"
+
+                if [ -f "$name" ]; then
+                    sha256sum $name >> $output
+                fi
+            done
+        done
+    )
+)
