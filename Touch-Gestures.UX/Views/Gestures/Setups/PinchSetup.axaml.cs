e36ec7a46b28dfb8cfd0312a8e264521380df6a3
Gess1t <tintamare91@gmail.com>
2024-10-09 00:49:01 +0200

    Potentially fix some warnings in a non-destructive way

--- Touch-Gestures.UX/Views/Gestures/Setups/PinchSetup.axaml.cs
+++ Touch-Gestures.UX/Views/Gestures/Setups/PinchSetup.axaml.cs
@@ -1,6 +1,8 @@
+using Avalonia.Controls;
+
 namespace TouchGestures.UX.Views.Gestures.Setups;
 
-public partial class RotateSetup : GestureSetup
+public partial class RotateSetup : UserControl
 {
     public RotateSetup()
     {
