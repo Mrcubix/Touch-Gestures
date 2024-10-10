58e0dcb64c86dd3743916635d7b171be4851e1c9
Gess1t <tintamare91@gmail.com>
2024-10-10 02:09:52 +0200

    Add some documentation to profiles

--- Touch-Gestures.Lib/Interfaces/IGesturesProfile.cs
+++ Touch-Gestures.Lib/Interfaces/IGesturesProfile.cs
@@ -6,14 +6,33 @@ namespace TouchGestures.Lib.Interfaces
 {
     public interface IGesturesProfile : IEnumerable<Gesture> 
     {
+        /// <summary>
+        ///   The name of the tablet associated with the profile.
+        /// </summary>
+        /// <remarks>
+        ///   "(Pen Only)" may be append to the name of the profile if it is a pen (Single Touch) profile. <br/>
+        ///   See <see cref="IsMultiTouch"/> for more information.
+        /// </remarks>
         [JsonProperty]
         string Name { get; }
 
+        /// <summary>
+        ///   Whether or not the profile is a multi-touch profile.
+        /// </summary>
+        /// <remarks>
+        ///   May also indicate that the profile is a pen (Single Touch) profile when false.
+        /// </remarks>
         [JsonProperty]
         bool IsMultiTouch { get; }
 
+        /// <summary>
+        ///   Add a gesture to the profile.
+        /// </summary>
         void Add(Gesture gesture);
 
+        /// <summary>
+        ///   Remove a gesture from the profile.
+        /// </summary>
         void Remove(Gesture gesture);
     }
 }
