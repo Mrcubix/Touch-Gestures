ef97ed344820f69c33dd94b02054bd1baf57e485
Gess1t <tintamare91@gmail.com>
2024-10-10 02:23:02 +0200

    Fix Some Codefactor Issues

--- Touch-Gestures.Tests/Tablet/GenericTouchReport.cs
+++ Touch-Gestures.Tests/Tablet/GenericTouchReport.cs
@@ -1,10 +1,11 @@
+using System;
 using OpenTabletDriver.Plugin.Tablet.Touch;
 
 namespace TouchGestures.Tests.Tablet
 {
     public class GenericTouchReport : ITouchReport
     {
-        public byte[] Raw { set; get; } = new byte[0];
-        public TouchPoint[] Touches { get; init; } = new TouchPoint[0];
+        public byte[] Raw { set; get; } = Array.Empty<byte>();
+        public TouchPoint[] Touches { get; init; } = Array.Empty<TouchPoint>();
     }
 }
