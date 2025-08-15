9a7aae74b365aef395db486541307199c3dfd20c
Gess1t <tintamare91@gmail.com>
2025-08-15 18:26:02 +0200

    Fix CI build by not using C#12 features when .NET 8 isn't installed

--- Touch-Gestures.Lib/Input/DeviceReportEventArgs.cs
+++ Touch-Gestures.Lib/Input/DeviceReportEventArgs.cs
@@ -3,17 +3,22 @@ using OpenTabletDriver.Plugin.Tablet.Touch;
 
 namespace TouchGestures.Lib.Input
 {
-    public class DeviceReportEventArgs(string name, ITouchReport report) : EventArgs
+    public class DeviceReportEventArgs : EventArgs
     {
+        public DeviceReportEventArgs(string name, ITouchReport report)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Report = report ?? throw new ArgumentNullException(nameof(report));
+        }
 
         /// <summary>
         ///   The name of the device
         /// </summary>
-        public string Name { get; set; } = name ?? throw new ArgumentNullException(nameof(name));
+        public string Name { get; set; }
 
         /// <summary>
         ///   The emitted or emulated touch report 
         /// </summary>
-        public ITouchReport Report { get; set; } = report ?? throw new ArgumentNullException(nameof(report));
+        public ITouchReport Report { get; set; }
     }
 }
